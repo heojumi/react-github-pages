89093828dd8322ea50773dfb72f1615868db5624
heojumi <hjm4630573@gmail.com>
2020-10-29 16:49:56 +0900

    first

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import data from '../yourdata';
+import profile from '../asset/Image/search.pstatic.jpg'
 
 class About extends Component {
     state = {  }
@@ -12,7 +13,7 @@ class About extends Component {
                     <p>{data.abouttext}</p>
             </Fade>
             </div>
-            {data.ShowAboutImage ? <img src={data.aboutImage} alt='about iamge'></img> : null}
+            <img src={profile} alt='about iamge' style={{height:500}}></img>
             
 
         </div>  );
--- src/yourdata.js
+++ src/yourdata.js
@@ -2,18 +2,18 @@ export default
     {
         //(Please Do Not Remove The comma(,) after every variable)
         //Change The Website Template
-        name :'chetanverma.',
+        name :'heojumi.',
         headerTagline: [//Line 1 For Header
-                        'Hi 👋 You found me! I am Your Name.',
+                        'Hi 👋 You found me! I am heojumi.',
                         //Line 2 For Header
-                        'Digital designer ',
+                        'student ',
                         //Line 3 For Header
-                        'from India'
+                        'from Korea'
     ],
         //Contact Email
         contactEmail:'hello@chetanverma.com',
         // Add Your About Text Here
-        abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        abouttext: "hi my name is heojumi I'm student  ",
         aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
        //Change This To Hide The Image of About Section (True Or False)
        ShowAboutImage:true,//true or false (Change Here)
